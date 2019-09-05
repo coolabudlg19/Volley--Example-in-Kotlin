1bb12350b1dc69b3e932841ebe677a6667f34d71
Assad Ginem <aginem7078@gmail.com>
2019-09-05 07:02:26 -0500

    Fixed 401 error on PUT and POST requests, as well as image loading

--- app/src/main/java/com/example/volleypractice0903/MainActivity.kt
+++ app/src/main/java/com/example/volleypractice0903/MainActivity.kt
@@ -5,45 +5,45 @@ import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), IVolley {
-    override fun onResponse(response: String) {
-        //Show
-        Toast.makeText(this@MainActivity,""+response,Toast.LENGTH_SHORT).show()
-    }
+        class MainActivity : AppCompatActivity(), IVolley {
+            override fun onResponse(response: String) {
+                //Show
+                Toast.makeText(this@MainActivity,""+response,Toast.LENGTH_SHORT).show()
+            }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
 
-        btn_get.setOnClickListener {
-            MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
-                .getRequest("https://jsonplaceholder.typicode.com/todos/1")
+                btn_get.setOnClickListener {
+                    MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
+                        .getRequest("https://jsonplaceholder.typicode.com/todos/1")
+                }
+
+                btn_post.setOnClickListener {
+                    MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
+                        .postRequest("https://jsonplaceholder.typicode.com/posts")
+                }
+
+                btn_put.setOnClickListener {
+                    MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
+                        .putRequest("https://jsonplaceholder.typicode.com/posts/1")
+                }
+
+                btn_patch.setOnClickListener {
+                    MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
+                        .patchRequest("https://jsonplaceholder.typicode.com/posts/1")
+                }
+
+                btn_delete.setOnClickListener {
+                    MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
+                        .deleteRequest("https://jsonplaceholder.typicode.com/posts/1")
+                }
+
+                btn_image_loader.setOnClickListener {
+                    image_view.setImageUrl("http://www.inspiredluv.com/wp-content/uploads/2016/10/24-cool-wallpapers.jpg"
+                        ,MyVolleyRequest(this@MainActivity).imageLoader)
+                }
+            }
+
         }
-
-        btn_post.setOnClickListener {
-            MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
-                .postRequest("https://jsonplaceholder.typicode.com/posts")
-        }
-
-        btn_put.setOnClickListener {
-            MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
-                .putRequest("https://jsonplaceholder.typicode.com/posts/1")
-        }
-
-        btn_patch.setOnClickListener {
-            MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
-                .patchRequest("https://jsonplaceholder.typicode.com/posts/1")
-        }
-
-        btn_delete.setOnClickListener {
-            MyVolleyRequest.getInstance(this@MainActivity, this@MainActivity)
-                .deleteRequest("https://jsonplaceholder.typicode.com/posts")
-        }
-
-        btn_image_loader.setOnClickListener {
-            image_view.setImageUrl("https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjvvNqF6bTkAhUFnKwKHYZ_B8wQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.aljazeera.com%2Findepth%2Fopinion%2Fpalestinian-history-180216202248594.html&psig=AOvVaw2M3oBMPYXSeCoiANwUajSZ&ust=1567605861109811"
-                    ,MyVolleyRequest.getInstance(this@MainActivity).imageLoader)
-        }
-    }
-
-}
